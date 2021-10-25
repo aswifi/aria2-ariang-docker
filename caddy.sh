2952235bee4e89cb1a5fb295f56f4a5471c3fcaa
KCYLion <1095756998@qq.com>
2021-10-25 23:58:37 +0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -1,16 +1,11 @@
 #! /bin/bash -eu
 
-echo "Run aria2c and ariaNG"
-
-echo "**** Generate basic auth password for caddy ****"
-ARIA2_PWD_ENCRYPT=$(caddy hash-password -plaintext ${ARIA2_PWD})
+echo "Run aria2c"
 
 case $ENABLE_AUTH in
 true)
   echo "Using Basic Auth config file "
   export CADDY_FILE=/usr/local/caddy/SecureCaddyfile
-  sed -i 's/ARIA2_USER/'"${ARIA2_USER}"'/g' ${CADDY_FILE}
-  sed -i 's/ARIA2_PWD_ENCRYPT/'"${ARIA2_PWD_ENCRYPT}"'/g' ${CADDY_FILE}
   ;;
 *)
   echo "Using caddy without Basic Auth"
